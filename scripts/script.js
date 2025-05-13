5e09bf21a99fd4c9a86790fb762b652c1f5f09aa
Hugovg2791 <hugovila2710@gmail.com>
2025-05-13 17:25:36 +0200

    cambios en js

--- scripts/script.js
+++ scripts/script.js
@@ -7,22 +7,24 @@ inputs.forEach(input => {
         const octeto3 = parseInt(document.getElementById('octeto3').value) || 0;
         const octeto4 = parseInt(document.getElementById('octeto4').value) || 0;
 
-        if (
-            octeto1 < 0 || octeto1 > 255 ||
-            octeto2 < 0 || octeto2 > 255 ||
-            octeto3 < 0 || octeto3 > 255 ||
-            octeto4 < 0 || octeto4 > 255
-        ) {
+        if (octeto1 < 0 || octeto1 > 255 ||octeto2 < 0 || octeto2 > 255 ||octeto3 < 0 || octeto3 > 255 || octeto4 < 0 || octeto4 > 255) {
             input.style.color = 'red';
+            input.style.borderColor = 'red';
+            input.style.boxShadow = '0 0 5px red';
         } else {
             input.style.color = 'green';
+            input.style.borderColor = 'green';
+            input.style.boxShadow = '0 0 5px red';
         }
     });
-});  
+});     
 
 
 document.getElementById('calcular').addEventListener('click', () => {
-
+    const octeto1 = parseInt(document.getElementById('octeto1').value) || '';
+    const octeto2 = parseInt(document.getElementById('octeto2').value) || '';
+    const octeto3 = parseInt(document.getElementById('octeto3').value) || '';
+    const octeto4 = parseInt(document.getElementById('octeto4').value) || '';
     const resultadoDiv = document.getElementById('resultado');
 
 
@@ -30,10 +32,12 @@ document.getElementById('calcular').addEventListener('click', () => {
         resultadoDiv.innerHTML = '<p class="error">Por favor, ingresa valores válidos en todos los campos.</p>';
         return;
     }
-    if (octeto1 < 0 || octeto1 > 255 || octeto2 < 0 || octeto2 > 255 || octeto3 < 0 || octeto3 > 255 || octeto4 < 0 || octeto4 > 255) {
+
+    if (octeto1 < 0 || octeto1 > 255 ||octeto2 < 0 || octeto2 > 255 ||octeto3 < 0 || octeto3 > 255 ||octeto4 < 0 || octeto4 > 255
+    ) {
         resultadoDiv.innerHTML = '<p class="error">Los valores de los octetos deben estar entre 0 y 255.</p>';
         return;
     }
 
-    
+    resultadoDiv.innerHTML = ''; 
 });
