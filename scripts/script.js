a4f893870adfc198ee5f972eb431fc1588ab6e23
lAIdrillin <gabrielgcastro1809@gmail.com>
2025-05-13 18:48:25 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -18,10 +18,9 @@ inputs.forEach(input => {
 
 
 document.getElementById('calcular').addEventListener('click', () => {
-    const octeto1 = parseInt(document.getElementById('octeto1').value) || '';
-    const octeto2 = parseInt(document.getElementById('octeto2').value) || '';
-    const octeto3 = parseInt(document.getElementById('octeto3').value) || '';
-    const octeto4 = parseInt(document.getElementById('octeto4').value) || '';
+    const octeto1 = parseInt(document.getElementById('octeto1').value);
+    const octeto2 = parseInt(document.getElementById('octeto2').value);
+    const octeto3 = parseInt(document.getElementById('octeto3').value);
     const resultadoDiv = document.getElementById('resultado');
 
 
