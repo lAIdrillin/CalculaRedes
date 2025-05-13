789fd64e672f12d46ee5ffee94599d305f8561b7
Hugovg2791 <hugovila2710@gmail.com>
2025-05-13 19:09:24 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -21,6 +21,7 @@ document.getElementById('calcular').addEventListener('click', () => {
     const octeto1 = parseInt(document.getElementById('octeto1').value);
     const octeto2 = parseInt(document.getElementById('octeto2').value);
     const octeto3 = parseInt(document.getElementById('octeto3').value);
+    const octeto4 = parseInt(document.getElementById('octeto4').value);
     const resultadoDiv = document.getElementById('resultado');
 
 
