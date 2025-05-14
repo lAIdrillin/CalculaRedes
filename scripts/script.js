9c826df092208b5d2d33cc2189e346d021b677e2
Hugovg2791 <hugovila2710@gmail.com>
2025-05-14 16:16:53 +0200

    cambios en js

--- scripts/script.js
+++ scripts/script.js
@@ -34,53 +34,49 @@ document.getElementById('calcular').addEventListener('click', () => {
         return;
     }
 
-    else if (octeto1 < 0 || octeto1 > 255 ||octeto2 < 0 || octeto2 > 255 ||octeto3 < 0 || octeto3 > 255 ||octeto4 < 0 || octeto4 > 255) {
-        resultadoDiv.innerHTML = '<p class="error">Los valores de los octetos deben estar entre 0 y 255.</p>';
-        return;
+    const [octeto1, octeto2, octeto3, octeto4] = octetos;
+    const ip = `${octeto1}.${octeto2}.${octeto3}.${octeto4}`;
+    let clase = '';
+    let mascara = '';
+    let direccion = '';
+
+    if(octeto1 >= 1 && octeto1 <= 126){
+        clase = 'Clase A';
+        mascara = '255.0.0.0';
+    }else if(octeto1 >= 128 && octeto1 <= 191){
+        clase = 'Clase B';
+        mascara = '255.255.0.0';
+    }else if(octeto1 >= 192 && octeto1 <= 223){
+        clase = 'Clase C';
+        mascara = '255.255.255.0';
+    }else if(octeto1 >= 224 && octeto1 <= 239){
+        clase = 'Clase D';
+        mascara = 'no tiene(multicast)';
+    }else if(octeto1 >= 240 && octeto1 <= 255){
+        clase = 'Clase E';
+        mascara = 'no tiene(experiemental)';
     }else{
-        const ip = `${octeto1}.${octeto2}.${octeto3}.${octeto4}`;
-        let clase = '';
-        if(octeto1 >= 1 && octeto1 <= 126){
-            clase = 'Clase A';
-            mascara = '255.0.0.0';
-        }else if(octeto1 >= 128 && octeto1 <= 191){
-            clase = 'Clase B';
-            mascara = '255.255.0.0';
-        }else if(octeto1 >= 192 && octeto1 <= 223){
-            clase = 'Clase C';
-            mascara = '255.255.255.0';
-        }else if(octeto1 >= 224 && octeto1 <= 239){
-            clase = 'Clase D';
-            mascara = 'no tiene(multicast)';
-        }else if(octeto1 >= 240 && octeto1 <= 255){
-            clase = 'Clase E';
-            mascara = 'no tiene(experiemental)';
-        }else{
-            clase = 'Clase desconocida';
-            mascara = 'no tiene';
-        }
-        if((octeto1 === 10) && (octeto2 >= 0 && octeto2 <= 255) && (octeto3 >= 0 && octeto3 <= 255) && (octeto4 >= 0 && octeto4 <= 255)){
-            direccion = 'Privada';
-        }
-       else if((octeto1 === 172) && (octeto2 >= 16 && octeto2 <= 31) && (octeto3 >= 0 && octeto3 <= 255) && (octeto4 >= 0 && octeto4 <= 255)){
-            direccion = 'Privada';
-        }
-        else if((octeto1 === 192) && (octeto2 === 168) && (octeto3 >= 0 && octeto3 <= 255) && (octeto4 >= 0 && octeto4 <= 255)){
-            direccion = 'Privada';
-        }
-        else{
-            direccion = 'Pública';
-        }
-
-        resultadoDiv.innerHTML = `<p>La dirección IP es <strong>${ip}</strong></p>
-                                  <p>Clase de red: <strong>${clase}</strong></p>
-                                  <p>Máscara por defecto: <strong>${mascara}</strong></p>
-                                  <p>Tipo de dirección: <strong>${direccion}</strong></p>`;
-                                  
+        clase = 'Clase desconocida';
+        mascara = 'no tiene';
     }
-
+    if((octeto1 === 10) && (octeto2 >= 0 && octeto2 <= 255) && (octeto3 >= 0 && octeto3 <= 255) && (octeto4 >= 0 && octeto4 <= 255)){
+        direccion = 'Privada';
+    }
+   else if((octeto1 === 172) && (octeto2 >= 16 && octeto2 <= 31) && (octeto3 >= 0 && octeto3 <= 255) && (octeto4 >= 0 && octeto4 <= 255)){
+        direccion = 'Privada';
+    }
+    else if((octeto1 === 192) && (octeto2 === 168) && (octeto3 >= 0 && octeto3 <= 255) && (octeto4 >= 0 && octeto4 <= 255)){
+        direccion = 'Privada';
+    }
+    else{
+        direccion = 'Pública';
+    }
+    mostrarVentanaEmergente(ip, clase, mascara, direccion);
+    resultadoDiv.innerHTML = `<p>La dirección IP es <strong>${ip}</strong></p>
+                              <p>Clase de red: <strong>${clase}</strong></p>
+                              <p>Máscara por defecto: <strong>${mascara}</strong></p>
+                              <p>Tipo de dirección: <strong>${direccion}</strong></p>`;
 });
-
 function mostrarVentanaEmergente(ip, clase, mascara, direccion) {
     const ventanaEmergente = document.createElement('div');
     ventanaEmergente.classList.add('ventana-emergente');
